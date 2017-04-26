4801eb71084ec2874d326e5c4a1dadc5800dbf1a
Nihal Singh <nihal.111@gmail.com>
2017-04-27 01:13:20 +0530

    statGenerator ready for work

--- snakesAndLadders/statGenerator.py
+++ snakesAndLadders/statGenerator.py
@@ -1,2 +1,9 @@
-from minThrows import modifiedDijkstra
+from minThrows import modifiedDijkstra, createBoard
 from minThrows2 import sequenceSolver
+import os
+
+if __name__ == "__main__":
+	for file in os.listdir("./100x100inputs"):
+	    if file.endswith(".txt"):
+	        board = createBoard(10001)
+	        sequenceSolver(10001, board)
