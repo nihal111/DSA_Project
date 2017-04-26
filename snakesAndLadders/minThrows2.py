0a6b928dcdfe95d37577907bf51fd6a042b8c310
Nihal Singh <nihal.111@gmail.com>
2017-04-26 21:03:43 +0530

    Added docs

--- snakesAndLadders/minThrows2.py
+++ snakesAndLadders/minThrows2.py
@@ -44,16 +44,16 @@ def solve():
 		for step in range (1,7):
 			if minStepsTo[sq-step]+1<minStepsTo[board[sq]]:
 				minStepsTo[board[sq]] = minStepsTo[sq-step]+1
-				prevSquareFor[board[sq]] = (sq-step, 6 - step)
+				prevSquareFor[board[sq]] = (sq-step, step)
 				changed = 1
-		# If minSteps for a square has been changed and it is a snake mouth
+		# If minSteps for a square(snake tail) has been changed and it is a snake mouth
 		# then update all blocks from the corresponding snake's tail to that snake's mouth
 		if changed == 1 and board[sq]<sq:
 			for sq2 in range (board[sq]+1,sq):
 				for step in range (1,7):
 					if minStepsTo[sq2-step]+1<minStepsTo[board[sq2]]:
 						minStepsTo[board[sq2]] = minStepsTo[sq2-step]+1
-						prevSquareFor[board[sq2]] = (sq2-step, 6 - step)
+						prevSquareFor[board[sq2]] = (sq2-step, step)
 	
 	# If path to 100 exists
 	if minStepsTo[100] < 101:
@@ -61,9 +61,9 @@ def solve():
 		x = 100
 		a = "100"
 		while minStepsTo[x]!=x:
-			a = a + " <-[" + str(prevSquareFor[x][1]) + "]- "+ str(prevSquareFor[x][0])
+			a = a + " --[" + str(prevSquareFor[x][1]) + "]-> "+ str(prevSquareFor[x][0])
 			x = prevSquareFor[x][0]
-		print a
+		print ' '.join(w[::-1] for w in a[::-1].split())
 	else:
 		print "No path found"
 
