1b4b955c239153a2ef656b42739fca7b0a592a2a
Srivatsan Sridhar <ssrivatsan97@gmail.com>
2017-04-26 18:59:21 +0530

    Updated new algo

--- snakesAndLadders/minThrows2.py
+++ snakesAndLadders/minThrows2.py
@@ -23,10 +23,26 @@ spath = [101 for i in range(0,101)]
 spathfrom = []
 
 def minpath(sq):
+	for sq in range (1,7):
+		spath[sq] = 1
+		spathfrom[sq] = 0
 	for sq in range (7,101):
 	changed=0
 		for step in range (1,7):
 			if spath[sq-step]+1<spath[go_to[sq]]:
 				spath[go_to[sq]] = spath[sq-step]+1
 				spathfrom[go_to[sq]] = sq-step
-				
+				changed = 1
+		if changed == 1 and go_to[sq]<sq:
+			for sq2 in range (go_to[sq]+1,sq):
+				for step in range (1,7):
+					if spath[sq2-step]+1<spath[go_to[sq2]]:
+						spath[go_to[sq2]] = spath[sq2-step]+1
+						spathfrom[go_to[sq2]] = sq2-step
+	print "Shortest path is " + str(spath[100]) + " steps."
+	x=100
+	a = ""
+	while x>0:
+		a = a + str(spathfrom[x]) + " -> "
+		x = spathfrom[x]
+	print a
